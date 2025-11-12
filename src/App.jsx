ce73e0e3ea026082aa62751b219eace92e6ead0f
ayoakeni <ayoakeni64@gmail.com>
2025-11-12 15:47:03 +0100

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import TherapistLogin from "./login/therapist_login";
 import TherapistDashboard from "./page/therapistDashboard";
 import AnonymousDashboard from "./page/anonymousDashboard";
 import Chatroom from "./page/chats_rooms/chatRoom";
+import Header from "./components/header"
 import "./styles/App.css";
 
 // -------------------------------------------------------------------
@@ -85,6 +86,7 @@ function AuthProvider({ children }) {
 export default function App() {
   return (
     <>
+      <Header/>
       <NotificationHandler />
       <AuthProvider>
         {({ user, isTherapist, loading }) => (
